4a60780d3c88b7669a13571a4b1d679799ae21bf
Jasminemaharjan <jasminemaharjan2418@gmail.com>
2026-08-23 23:44:58 +0545

    WIP-Homepage

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import Carousel from '../components/Carousel';
+import Card from '../components/Card';
 
 export default function Home() {
   return (
     <div className='mainpage'>
-        <div> <Navbar/> </div>
-        <div>Body</div>
-        <div> <Footer/> </div>
+      <div> <Navbar /> </div>
+      <div><Carousel/></div>
+      <div><Card/></div>
+      <div> <Footer /> </div>
     </div>
   )
 }
